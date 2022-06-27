dcef4e0640251a4fab6d4bac51644ada3927719f
lg0leg <lg.oleg1067@gmail.com>
2022-06-28 00:14:11 +0300

    feat: add slide in on scroll

--- js/gallery.js
+++ js/gallery.js
@@ -1,37 +1,89 @@
 function randomPic() {
-    let pics = [
-        "./assets/img/galery/galery1.jpg",
-        "./assets/img/galery/galery2.jpg",
-        "./assets/img/galery/galery3.jpg",
-        "./assets/img/galery/galery4.jpg",
-        "./assets/img/galery/galery5.jpg",
-        "./assets/img/galery/galery6.jpg",
-        "./assets/img/galery/galery7.jpg",
-        "./assets/img/galery/galery8.jpg",
-        "./assets/img/galery/galery9.jpg",
-        "./assets/img/galery/galery10.jpg",
-        "./assets/img/galery/galery11.jpg",
-        "./assets/img/galery/galery12.jpg",
-        "./assets/img/galery/galery13.jpg",
-        "./assets/img/galery/galery14.jpg",
-        "./assets/img/galery/galery15.jpg"
-      ];
+  let pics = [
+    './assets/img/galery/galery1.jpg',
+    './assets/img/galery/galery2.jpg',
+    './assets/img/galery/galery3.jpg',
+    './assets/img/galery/galery4.jpg',
+    './assets/img/galery/galery5.jpg',
+    './assets/img/galery/galery6.jpg',
+    './assets/img/galery/galery7.jpg',
+    './assets/img/galery/galery8.jpg',
+    './assets/img/galery/galery9.jpg',
+    './assets/img/galery/galery10.jpg',
+    './assets/img/galery/galery11.jpg',
+    './assets/img/galery/galery12.jpg',
+    './assets/img/galery/galery13.jpg',
+    './assets/img/galery/galery14.jpg',
+    './assets/img/galery/galery15.jpg',
+  ];
 
-      function shuffle(array) {
-        array.sort(() => Math.random() - 0.5);
-      }
+  function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
 
-      shuffle(pics);
+  shuffle(pics);
 
-      pics.map(function(el) {
-        const pictureInnerContainer = document.querySelector('.picture-inner-container');
-        const img = document.createElement('img');
-        img.classList.add('gallery-item')
-        img.src = el;
-        img.alt = `galery1`;
-        // img.width = '456';
-        pictureInnerContainer.append(img);
-      })
-}  
+  pics.map(function (el) {
+    const pictureInnerContainer = document.querySelector(
+      '.picture-inner-container'
+    );
+    const img = document.createElement('img');
+    img.classList.add('gallery-item');
+    img.classList.add('slide-in');
+    img.src = el;
+    img.alt = `galery1`;
+    // img.width = '456';
+    pictureInnerContainer.append(img);
+  });
+}
 
-randomPic()
+randomPic();
+
+//плавное появление при прокрутке
+
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+function debounce(f, ms = 100) {
+  let isCooldown = false;
+
+  return function () {
+    if (isCooldown) return;
+    f.apply(this, arguments);
+    isCooldown = true;
+    setTimeout(() => (isCooldown = false), ms);
+  };
+}
+
+const displayScrollElement = (element) => {
+  element.classList.add('active');
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove('active');
+};
+
+const elementInView = (el, percentageScroll = 100) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) *
+      (percentageScroll / 100)
+  );
+};
+
+const handleScrollAnimation = () => {
+  galleryItems.forEach((el) => {
+    if (elementInView(el, 100)) {
+      displayScrollElement(el);
+    } else {
+      hideScrollElement(el);
+    }
+  });
+};
+
+window.addEventListener('scroll', () => {
+  handleScrollAnimation();
+});
+
+// window.addEventListener('scroll', debounce(handleScrollAnimation));
